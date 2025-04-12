deef4ab0d3bdc1f7324b943b4c8802e0eda1eb1f
Demzel-116 <ahmadulinn@yandex.ru>
2025-04-12 12:39:02 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
-  int blue_balls[5];
-  int size = sizeof(blue_balls) / sizeof(blue_balls[0]);
-  printf("В коробке blue_balls помещается %d шариков.\n", size);
+  int blue_balls[5] = {2,2,2,2,2}; // 5 синих шариков
+    int blue_count = sizeof(blue_balls) / sizeof(blue_balls[0]);
+    printf("Количество синих шариков: %d\n", blue_count);
   return 0;
 }
